e319b9c35e2f9f83eb9724a96bbc3872d31d297d
DEV <lucas.m3dina@gmail.com>
2021-06-25 11:03:28 -0300

    header

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Senai_InstaDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
