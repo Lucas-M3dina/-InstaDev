8dd6f94449323b62e54ea622d152f39a1b2333fc
Lucas-M3dina <Lucas.m3dina@gmail.com>
2021-07-07 01:22:34 -0300

    Main

---
+++ Controllers/Editar_PerfilController.cs
@@ -0,0 +1,97 @@
+
+using System.Collections.Generic;
+using System.IO;
+using InstaDev.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstaDev.Controllers
+{
+        [Route("EditPerfil")]
+    public class Editar_PerfilController : Controller
+    {
+        Usuario Usuario = new Usuario();
+        Usuario UsuarioModel = new Usuario();
+        // Random IdAleatorio = new Random();
+        bool repetir;
+
+
+        [Route("Listar")]
+        public IActionResult Index(){
+            ViewBag.UserName = HttpContext.Session.GetString("_UserName");
+            ViewBag.Name = HttpContext.Session.GetString("_Nome");
+            ViewBag.ImagemUsuario = HttpContext.Session.GetString("_FotoUsuario");
+            // ViewBag.UsuarioModel = Usuario.LerTodos();
+            ViewBag.Usuario = UsuarioModel.LerTodos();
+            return View();
+        }
+
+
+       [Route("Modificar")]
+        public IActionResult Modificar(IFormCollection form){
+            int idUser = int.Parse(HttpContext.Session.GetString("_IdUser"));
+            Usuario NovoUsuario = new Usuario();
+            Usuario AntigoUsuario = new Usuario();
+            string nick = HttpContext.Session.GetString("_UserName");
+
+            List<Usuario> usuarios = new List<Usuario>();
+            usuarios = Usuario.LerTodos();
+            AntigoUsuario = usuarios.Find(x => x.NomeUsuario == nick);
+
+
+            if (form.Files.Count > 0)
+            {
+                //Upload inicio
+
+                var file = form.Files[0];
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                NovoUsuario.FotoPerfil = file.FileName;
+
+
+            }else
+            {
+                NovoUsuario.FotoPerfil = "padrao.png";
+            }
+            
+            
+            NovoUsuario.FotoPerfil = AntigoUsuario.FotoPerfil;
+            NovoUsuario.Senha = AntigoUsuario.Senha;
+            NovoUsuario.IdUsuario = AntigoUsuario.IdUsuario;
+            NovoUsuario.Nome = form["Nome"];
+            NovoUsuario.NomeUsuario = form["Username"];
+            NovoUsuario.Email = form["Email"];
+
+            UsuarioModel.Deletar(idUser);
+            UsuarioModel.Criar(NovoUsuario);
+            ViewBag.Usuario = UsuarioModel.LerTodos();
+
+            return LocalRedirect("~/Feed");
+        }
+        
+        [Route("Excluir")]
+        public IActionResult Deletar()
+        {
+            int IdDel = int.Parse(HttpContext.Session.GetString("_IdUser"));
+            UsuarioModel.Deletar(IdDel);
+            ViewBag.Usuario = UsuarioModel.LerTodos();
+            return LocalRedirect("~/");
+        }
+
+        
+    }
+    
+
+}
